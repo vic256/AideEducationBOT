610bba030186f1858271e50c9c5ed3bc4a1684a1
Vic256 <contact.vic256@gmail.com>
2019-11-11 15:36:00 +0100

    GIF search to giphy

--- Commands/fun/gif.js
+++ Commands/fun/gif.js
@@ -1,5 +1,8 @@
 const gifSearch = require("gif-search");
 const Discord = require("discord.js");
+const giphy = require('giphy-api')('k0t2AnD8lWceA8SFGJ2mQIcitQ4r2vyR');
+const fetchGifs = require('fetch-gifs');
+const request = require('request');
 module.exports = {
   name: 'gif',
   description: 'Rechercher un GIF',
@@ -9,15 +12,24 @@ module.exports = {
   args: true,
   guildOnly: false,
   execute(message, args, bot, embedfooter) {
-    gifSearch.random(args[0]).then(
-         gifUrl => {
-
-         let randomcolor = ((1 << 24) * Math.random() | 0).toString(16) //Optional
-         var embed = new Discord.RichEmbed()
-             .setColor(`#${randomcolor}`)
-             .setImage(gifUrl)
-             .setFooter(embedfooter + "| GIF")
-         message.channel.send(embed);
-     });
+    let term = args.join(' ')
+    term = encodeURI(term)
+    request('http://api.giphy.com/v1/gifs/search?q='+ term + '&rating=r&api_key=k0t2AnD8lWceA8SFGJ2mQIcitQ4r2vyR', function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        let content =  JSON.parse(body)
+        let item = Math.floor(Math.random() * content.data.length)
+        if(!content.data[item]) return message.reply("Oh la la ! J'ai cherché partout dans ma grande bibliothèque mais je ne trouve pas votre gif... :cry: ");
+        let gif = content.data[item].images.fixed_height.url
+        
+        message.channel.send("", {
+        embed: new Discord.RichEmbed()
+          .setTitle("Recherche de gif")
+          .setDescription("Recherche : " + term)
+          .setImage(gif)
+          .setFooter(embedfooter + " | GIF")
+          .setColor("#fffff")
+          });
+        }
+         })
   },
 };
