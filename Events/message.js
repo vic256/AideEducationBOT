ba96aa3a1a89de6efd75857e818ee83a18cfea27
Vic256 <contact.vic256@gmail.com>
2019-12-26 18:50:22 +0100

    Athor -> Author

--- Events/message.js
+++ Events/message.js
@@ -2,5 +2,5 @@ module.exports = (bot, message) => {
     if (message.author.bot) return;
 	const args = message.content.split(" ");
 	let text = args.slice(0).join(" ");
-	if(message.channel.type === 'dm') return console.log(`--> Message privé reçu <--\nUtilisateur : ${message.athor.username}#${message.author.discriminator} - ${message.author.id}\nMessage : ${text}`);
+	if(message.channel.type === 'dm') return console.log(`--> Message privé reçu <--\nUtilisateur : ${message.author.username}#${message.author.discriminator} - ${message.author.id}\nMessage : ${text}`);
 };
